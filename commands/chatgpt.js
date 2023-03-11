2d7ffe8637c721a8e7437eecaf90038f82c2957a
Niklas Malkusch <niklasmalkusch@googlemail.com>
2023-03-11 18:40:19 +0100

    Added chatgpt command

--- commands/chatgpt.js
+++ commands/chatgpt.js
@@ -1,26 +1,40 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
+import moment from "moment";
 import { ChatGPTAPI } from 'chatgpt';
 import fetch from 'node-fetch';
+
 import config from '../config.json' assert { type: "json" };
 const openaiKey = config.openaiKey;
 
-const question = 'What is the meaning of life?';
+const log = x => { console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${x}`) };
 
-async function askQuestion(question) {
+async function sendPrompt(prompt) {
+  log('Fetching response from OpenAI with prompt: ' + prompt);
   const api = new ChatGPTAPI({
     apiKey: openaiKey,
     fetch: fetch
   });
 
-  const response = await api.sendMessage(question);
+  const response = await api.sendMessage(prompt);
+  log('Response: ' + response.text);
   return response.text;
 }
 
 export default {
-    data: new SlashCommandBuilder()
-        .setName('chatgpt')
-        .setDescription('Replies with the power of GPT-3!'),
-    async execute(interaction) {
-        await interaction.reply('Hello ChatGPT!');
+  data: new SlashCommandBuilder()
+    .setName('chatgpt')
+    .setDescription('Answers a prompt using the OpenAI ChatGPT API.')
+    .addStringOption(option => 
+      option.setName('prompt')
+      .setDescription('The prompt to answer.')),
+  async execute(interaction) {
+    const prompt = interaction.options.getString('prompt');
+    try {
+      const response = await sendPrompt(prompt);
+      await interaction.reply(response);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply('There was an error while processing your request.');
     }
+  }
 }
